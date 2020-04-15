8b5377d06b013377d425c7e2d1dbb33cbb4ada63
mulvaneya7 <mulvaneya7@gmail.com>
2020-04-15 11:48:08 -0700

    edit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if(!dev) {
     app.use(express.static(path.resolve(__dirname, 'build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.js'))
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
 
 }
