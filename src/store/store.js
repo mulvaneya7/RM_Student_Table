85943f1d3cba5ba79ded55a90b7edcc4b350fd46
mulvaneya7 <mulvaneya7@gmail.com>
2020-04-15 12:10:39 -0700

    devTool

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,12 @@ import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import { reducer as formReducer } from "redux-form";
 import thunkMiddleware from 'redux-thunk'; 
 import tableReducer from '../store/tableReducer';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const rootReducer = combineReducers({
   form: formReducer,
   table: tableReducer
 });
 
-export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export default createStore(rootReducer, compose(applyMiddleware(thunkMiddleware)));
